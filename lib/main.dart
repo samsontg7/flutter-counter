226d347fc295e84eb9fdf438226f3ed429416205
samsontg7 <samsondereje7@gmail.com>
2024-05-14 04:27:15 +0200

    counter app

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'counter demo',
+      home: HomePageState(),
+    ),
+  );
+}
+
+class HomePageState extends StatefulWidget {
+  const HomePageState({super.key});
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<HomePageState> {
+  int counter = 0;
+  void increamentCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decreamentCounter() {
+    setState(() {
+      counter--;
+    });
+  }
+
+  void clearbutton() {
+    setState(() {
+      counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'counter demo',
+          style: TextStyle(color: Colors.white, fontSize: 30.3),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/sh.jpg'),
+                scale: 1.0,
+                fit: BoxFit.cover)),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("counter  $counter"),
+              FloatingActionButton(
+                  child: Text("Clear"), onPressed: clearbutton),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: decreamentCounter,
+            tooltip: 'decreament',
+            child: const Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            onPressed: increamentCounter,
+            tooltip: 'increament',
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
